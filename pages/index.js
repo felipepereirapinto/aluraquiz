ff1ef8368126785ab410550daaf0e7127080207b
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2021-01-26 14:41:24 -0300

    eslint + prettier

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -22,8 +22,8 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  const router = useRouter()
-  const [name, setName] = React.useState('')
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
   return (
     <QuizBackground backgroundImage={db.bg}>
@@ -33,24 +33,22 @@ export default function Home() {
 
       <QuizContainer>
         <QuizLogo />
-        
+
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
-          
+
           <Widget.Content>
-            <form onSubmit={ function (event) {
-              event.preventDefault()
-              router.push(`quiz?name=${name}`)
-            }}
+            <form
+              onSubmit={function (event) {
+                event.preventDefault();
+                router.push(`quiz?name=${name}`);
+              }}
             >
               <input
-                // onChange={function (event) {
-                //   setName(event.target.value)
-                // }}
-                onChange={event => setName(event.target.value)}
-                placeholder="Diz aí seu nome pra jogar :)" 
+                onChange={(event) => setName(event.target.value)}
+                placeholder="Diz aí seu nome pra jogar :)"
               />
               <button type="submit" disabled={name.length === 0}>
                 Jogar {name}
